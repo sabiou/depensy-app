64ee3a005c5d9f768bfd15b48551250a3e8c5636
Farouk <sfarouk298@gmail.com>
2018-11-26 01:34:28 +0100

    Add : Category model

--- app/src/main/java/xyz/godi/budgetmanager/model/Category.java
+++ app/src/main/java/xyz/godi/budgetmanager/model/Category.java
@@ -1,5 +1,45 @@
 package xyz.godi.budgetmanager.model;
 
-public class Category {
+import android.arch.persistence.room.ColumnInfo;
+import android.os.Parcel;
+import android.os.Parcelable;
 
+public class Category implements Parcelable {
+    @ColumnInfo(name = "name")
+    private String name;
+    @ColumnInfo(name = "description")
+    private String description;
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    protected Category(Parcel in) {
+        name = in.readString();
+        description = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(description);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Category> CREATOR = new Creator<Category>() {
+        @Override
+        public Category createFromParcel(Parcel in) {
+            return new Category(in);
+        }
+
+        @Override
+        public Category[] newArray(int size) {
+            return new Category[size];
+        }
+    };
 }
