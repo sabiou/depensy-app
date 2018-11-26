dd6862fb7d64598f0ac804b64a0f04ab7130a88a
Farouk <sfarouk298@gmail.com>
2018-11-26 01:27:37 +0100

    Added Parcelable implementation

--- app/src/main/java/xyz/godi/budgetmanager/model/Budget.java
+++ app/src/main/java/xyz/godi/budgetmanager/model/Budget.java
@@ -2,11 +2,13 @@ package xyz.godi.budgetmanager.model;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import java.util.Date;
 
 @Entity(tableName = "budget_table")
-public class Budget {
+public class Budget implements Parcelable {
     @ColumnInfo(name = "name")
     private String name;
     @ColumnInfo(name = "amount")
@@ -22,6 +24,23 @@ public class Budget {
         this.startDate = startDate;
         this.endDate = endDate;
     }
+
+    protected Budget(Parcel in) {
+        name = in.readString();
+        amount = in.readFloat();
+    }
+
+    public static final Creator<Budget> CREATOR = new Creator<Budget>() {
+        @Override
+        public Budget createFromParcel(Parcel in) {
+            return new Budget(in);
+        }
+
+        @Override
+        public Budget[] newArray(int size) {
+            return new Budget[size];
+        }
+    };
 
     public String getName() {
         return name;
@@ -53,5 +72,16 @@ public class Budget {
 
     public void setEndDate(Date endDate) {
         this.endDate = endDate;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeFloat(amount);
     }
 }
