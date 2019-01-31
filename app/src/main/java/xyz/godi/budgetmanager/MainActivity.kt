33e18bfe9530e23cbd304054c5a181849e11adb6
Farouk <sfarouk298@gmail.com>
2019-01-31 18:31:43 +0100

    ADDED : LauncherActivity.kt in app module

--- app/src/main/java/xyz/godi/budgetmanager/MainActivity.kt
+++ app/src/main/java/xyz/godi/budgetmanager/MainActivity.kt
@@ -11,20 +11,8 @@ import xyz.godi.budgetmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG: String = MainActivity::class.java.simpleName
-    private lateinit var drawerLayout: DrawerLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        drawerLayout = binding.drawerLayout
-
-        val navController = this.findNavController(R.id.navHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.navHostFragment)
-        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
