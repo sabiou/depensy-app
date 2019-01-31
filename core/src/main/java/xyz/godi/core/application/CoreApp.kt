89ecb973a9efeb94841d9535e1b7619e5b8a7c6f
Farouk <sfarouk298@gmail.com>
2019-01-31 18:20:07 +0100

    Update : CoreApp.kt

--- core/src/main/java/xyz/godi/core/application/CoreApp.kt
+++ core/src/main/java/xyz/godi/core/application/CoreApp.kt
@@ -4,4 +4,13 @@ import android.app.Application
 
 open class CoreApp : Application() {
     // Implementation to come
+
+    override fun onCreate() {
+        super.onCreate()
+        initDI()
+    }
+
+    private fun initDI() {
+        // TO be implemented
+    }
 }
