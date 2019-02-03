d2de5868ec52db0b8ab2acad6ffa7f3a8498e984
Farouk <sfarouk298@gmail.com>
2019-02-03 20:16:13 +0100

    Add: Dagger module providers

--- app/src/main/java/xyz/godi/budgetmanager/di/AppModule.kt
+++ app/src/main/java/xyz/godi/budgetmanager/di/AppModule.kt
@@ -3,9 +3,13 @@ package xyz.godi.budgetmanager.di
 import android.app.Application
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
 import dagger.Module
 import dagger.Provides
 import xyz.godi.budgetmanager.db.ExpensesDb
+import xyz.godi.expenses.db.DailyExpenseDao
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 import javax.inject.Singleton
 
 @Module
@@ -22,5 +26,17 @@ class AppModule {
     @Singleton
     fun provideDataBase(context: Context): ExpensesDb {
         return Room.databaseBuilder(context, ExpensesDb::class.java, ExpensesDb.DATABASE_NAME).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesDailyExpensesDao(database: ExpensesDb): DailyExpenseDao {
+        return database.dailyExpenseDao()
+    }
+
+    @Provides
+    @Singleton
+    fun providesExecutor(): Executor {
+        return Executors.newSingleThreadExecutor()
     }
 }
