9c8ef320f145a4f6ac88843ff9d8d2083db524f6
Farouk <sfarouk298@gmail.com>
2019-02-03 16:17:57 +0100

    Add: DailyExpenseDao.kt methods

--- expenses/src/main/java/xyz/godi/expenses/db/DailyExpenseDao.kt
+++ expenses/src/main/java/xyz/godi/expenses/db/DailyExpenseDao.kt
@@ -1,5 +1,17 @@
 package xyz.godi.expenses.db
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import xyz.godi.expenses.data.Expense
+
+@Dao
 interface DailyExpenseDao {
-    // to be implemented
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(expense: List<Expense>)
+
+    @Query("SELECT * FROM expenses")
+    fun load(expense: Expense): LiveData<List<Expense>>
 }
