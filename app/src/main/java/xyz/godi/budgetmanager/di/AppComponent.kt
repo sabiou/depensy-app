d31422a38e536b6311b5d4ddf03ecdfccd112bc2
Farouk <sfarouk298@gmail.com>
2019-02-09 18:18:59 +0100

    Fix: AppComponent.kt

--- app/src/main/java/xyz/godi/budgetmanager/di/AppComponent.kt
+++ app/src/main/java/xyz/godi/budgetmanager/di/AppComponent.kt
@@ -1,14 +1,15 @@
 package xyz.godi.budgetmanager.di
 
 import android.app.Application
+import androidx.core.view.KeyEventDispatcher
 import dagger.BindsInstance
 import dagger.Component
 
 @Component(modules = [AppModule::class])
-class AppComponent {
+interface AppComponent {
+
     @Component.Builder
     interface Builder {
-
         @BindsInstance
         fun application(application: Application): Builder
 
