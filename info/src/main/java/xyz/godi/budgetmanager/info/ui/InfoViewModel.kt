16c7b72b0fbd96b0ce227c53d79c8deef1f97bcf
Farouk <sfarouk298@gmail.com>
2019-02-12 12:25:36 +0100

    Added InfoViewModel.kt and InfoFragment.kt

---
+++ info/src/main/java/xyz/godi/budgetmanager/info/ui/InfoViewModel.kt
@@ -0,0 +1,6 @@
+package xyz.godi.budgetmanager.info.ui
+
+import xyz.godi.budgetmanager.core.viewmodel.ReselectViewModel
+import javax.inject.Inject
+
+class InfoViewModel @Inject constructor(): ReselectViewModel()
