48c63226dc71ddd3edd2a5582f858cfd088e9465
Farouk <sfarouk298@gmail.com>
2019-02-12 12:27:45 +0100

    add StatsViewModel.kt

---
+++ app/src/main/java/xyz/godi/budgetmanager/ui/StatsViewModel.kt
@@ -0,0 +1,6 @@
+package xyz.godi.budgetmanager.ui
+
+import xyz.godi.budgetmanager.core.viewmodel.ReselectViewModel
+import javax.inject.Inject
+
+class StatsViewModel @Inject constructor(): ReselectViewModel()
