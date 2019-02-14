e3b221a566e4b8dc7c6a510f560d9d87f00d52fd
Farouk <sfarouk298@gmail.com>
2019-02-14 18:23:01 +0100

    add AddExpenseFragment.kt

--- app/src/main/java/xyz/godi/budgetmanager/ui/AddExpenseFragment.kt
+++ app/src/main/java/xyz/godi/budgetmanager/ui/AddExpenseFragment.kt
@@ -2,7 +2,6 @@ package xyz.godi.budgetmanager.ui
 
 
 import android.os.Bundle
-import android.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +13,6 @@ class AddExpenseFragment : DaggerFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_expense, container, false)
     }
 }
