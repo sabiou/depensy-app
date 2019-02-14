c9d9b62cfa170c1ff8dbfa8a4ddff041b9f50c0e
Farouk <sfarouk298@gmail.com>
2019-02-14 18:20:24 +0100

    Added StatsFragment.kt

--- app/src/main/java/xyz/godi/budgetmanager/ui/StatsFragment.kt
+++ app/src/main/java/xyz/godi/budgetmanager/ui/StatsFragment.kt
@@ -1,16 +1,19 @@
 package xyz.godi.budgetmanager.ui
 
-import androidx.annotation.ContentView
-import androidx.lifecycle.ViewModelProvider
-import dagger.android.support.DaggerFragment
-import xyz.godi.budgetmanager.R
-import javax.inject.Inject
 
-@ContentView
-(R.layout.fragment_stats)
+import android.os.Bundle
+import android.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.android.support.DaggerFragment
+
+import xyz.godi.budgetmanager.R
+
 class StatsFragment : DaggerFragment() {
 
-    @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_stats, container, false)
+    }
 }
