0f64a7fa6c6cf45b33d4f0e5aee0b519e262d537
Farouk <sfarouk298@gmail.com>
2019-02-26 05:17:13 +0100

    updated the AppComponent.kt class

--- app/src/main/java/xyz/godi/budgetmanager/di/AppComponent.kt
+++ app/src/main/java/xyz/godi/budgetmanager/di/AppComponent.kt
@@ -3,13 +3,17 @@ package xyz.godi.budgetmanager.di
 import android.app.Application
 import dagger.BindsInstance
 import dagger.Component
+import dagger.android.AndroidInjectionModule
 import xyz.godi.budgetmanager.ExpensesManagerApplication
+import javax.inject.Singleton
 
-@Component(modules = [AppModule::class])
+@Singleton
+@Component(modules = [AndroidInjectionModule::class, AppModule::class])
 interface AppComponent {
 
     @Component.Builder
     interface Builder {
+
         @BindsInstance
         fun application(application: Application): Builder
 
