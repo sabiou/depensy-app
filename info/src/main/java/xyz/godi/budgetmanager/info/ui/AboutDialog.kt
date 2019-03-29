65d267ea3afd095816ff3749e25494d04aaec8fb
Farouk <sfarouk298@gmail.com>
2019-03-29 03:26:14 +0100

    add About this app dialog

---
+++ info/src/main/java/xyz/godi/budgetmanager/info/ui/AboutDialog.kt
@@ -0,0 +1,38 @@
+package xyz.godi.budgetmanager.info.ui
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.afollestad.materialdialogs.MaterialDialog
+import xyz.godi.budgetmanager.info.R
+import java.lang.IllegalStateException
+
+class AboutDialog: DialogFragment() {
+
+    companion object {
+        // TAG
+        private const val TAG = "ABOUT_DIALOG"
+
+        // show the dialog inside of the context
+        fun show(context: InfoFragment) {
+            val dialog = AboutDialog()
+
+            dialog.show(
+                    context.childFragmentManager,
+                    TAG
+            )
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val context = activity ?: throw IllegalStateException("ohh pas ça")
+
+        return MaterialDialog(context)
+                .title(text = getString(R.string.about_title))
+                .message(
+                        res = R.string.about_body,
+                        html = true,
+                        lineHeightMultiplier = 1.4f
+                )
+    }
+}
