ae6db78c1ab59f5b115c777c6a71e3c951ebe3b1
Farouk <sfarouk298@gmail.com>
2019-03-29 02:00:14 +0100

    update

---
+++ app/src/main/java/xyz/godi/budgetmanager/screens/expenses/ExpensesViewModel.kt
@@ -0,0 +1,8 @@
+package xyz.godi.budgetmanager.screens.expenses
+
+import androidx.lifecycle.ViewModel
+import xyz.godi.budgetmanager.data.repository.ExpensesRepository
+
+class ExpensesViewModel(repository: ExpensesRepository): ViewModel() {
+
+}
