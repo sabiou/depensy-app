d0853f33da0ddc55aab899452fc6800206a47970
Farouk <sfarouk298@gmail.com>
2019-03-29 02:01:27 +0100

    updated

---
+++ app/src/main/java/xyz/godi/budgetmanager/screens/stats/StatsViewModel.kt
@@ -0,0 +1,8 @@
+package xyz.godi.budgetmanager.screens.stats
+
+import androidx.lifecycle.ViewModel
+import xyz.godi.budgetmanager.data.repository.StatsRepository
+
+class StatsViewModel(repository: StatsRepository): ViewModel() {
+
+}
