cf19853c6f7ca81f376394c7b6393fbc43fe7d89
Farouk <sfarouk298@gmail.com>
2019-04-01 05:24:45 +0100

    Utilities file

---
+++ app/src/main/java/xyz/godi/budgetmanager/Utils.kt
@@ -0,0 +1,19 @@
+package xyz.godi.budgetmanager
+
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+
+/**
+ * Take the Long milliseconds returned by the system and stored in Room,
+ * and convert it to a nicely formatted string for display.
+ *
+ * EEEE - Display the long letter version of the weekday
+ * MMM - Display the letter abbreviation of the nmotny
+ * dd-yyyy - day in month and full year numerically
+ * HH:mm - Hours and minutes in 24hr format
+ */
+@SuppressLint("SimpleDateFormat")
+fun convertLongToDateString(systemTime: Long): String {
+    return SimpleDateFormat("EEEE MMM-dd-yyyy HH:mm")
+            .format(systemTime).toString()
+}
