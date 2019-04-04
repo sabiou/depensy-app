6b52a51f5740760591ae7bbf26134d567a3b9556
Farouk <sfarouk298@gmail.com>
2019-04-04 05:35:57 +0100

    somes changes

---
+++ app/src/main/java/xyz/godi/budgetmanager/ui/history/HistoryViewModel.kt
@@ -0,0 +1,6 @@
+package xyz.godi.budgetmanager.ui.history
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class HistoryViewModel(application: Application): AndroidViewModel(application)
