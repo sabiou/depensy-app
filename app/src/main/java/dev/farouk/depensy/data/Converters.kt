458c11699a108ab17e05f6842b7beecb38408fa5
Farouk <sfarouk298@gmail.com>
2019-04-06 19:29:55 +0100

    Add database date class

---
+++ app/src/main/java/dev/farouk/depensy/data/Converters.kt
@@ -0,0 +1,19 @@
+package dev.farouk.depensy.data
+
+import androidx.room.TypeConverter
+import java.util.*
+
+class Converters {
+    // If we want to persist instances of Date, we can write the following TypeConverter to store
+    // the equivalent Unix timestamp in the database.
+
+    @TypeConverter
+    fun fromTimeStamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimeStamp(date: Date?): Long? {
+        return date?.time
+    }
+}
