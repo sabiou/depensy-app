d48fceece11a3ac55bf0ad86084d70dca994e3cd
Farouk <sfarouk298@gmail.com>
2019-12-10 16:34:19 +0100

    fixed ViewModelModule

--- core/src/main/java/dev/farouk/depensy/core/di/ViewModelModule.kt
+++ core/src/main/java/dev/farouk/depensy/core/di/ViewModelModule.kt
@@ -1,2 +1,17 @@
 package dev.farouk.depensy.core.di
 
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.Module
+
+/**
+ * Module used to define the connection between the framework's [ViewModelProvider.Factory] and
+ * our own implementation: [IOSchedViewModelFactory].
+ */
+@Module
+@Suppress("UNUSED")
+abstract class ViewModelModule {
+    @Binds
+    internal abstract fun bindViewModelFactory(factory: DepensyViewModelFactory):
+            ViewModelProvider.Factory
+}
