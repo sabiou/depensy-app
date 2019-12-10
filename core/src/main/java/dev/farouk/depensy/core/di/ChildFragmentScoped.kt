267e05d0e6706fd74ba288e1f402b074a9ae1f94
Farouk <sfarouk298@gmail.com>
2019-12-10 16:43:20 +0100

    Add ChildFragmentScoped

---
+++ core/src/main/java/dev/farouk/depensy/core/di/ChildFragmentScoped.kt
@@ -0,0 +1,13 @@
+package dev.farouk.depensy.core.di
+
+import javax.inject.Scope
+
+/**
+ * The ChildFragmentScoped custom scoping annotation specifies that the lifespan of a dependency be
+ * the same as that of a child Fragment. This is used to annotate dependencies that behave like a
+ * singleton within the lifespan of a child Fragment
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class ChildFragmentScoped
