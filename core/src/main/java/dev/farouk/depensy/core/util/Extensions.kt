112447eaf822c5f88e2b85e342f4944a81088723
Farouk <sfarouk298@gmail.com>
2019-12-10 10:36:28 +0100

    Add some extensions functions

---
+++ core/src/main/java/dev/farouk/depensy/core/util/Extensions.kt
@@ -0,0 +1,51 @@
+package dev.farouk.depensy.core.util
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+/**
+ * Allows calls like
+ *
+ * `viewGroup.inflate(R.layout.foo)`
+ */
+fun ViewGroup.inflate(@LayoutRes layout: Int, attachToRoot: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layout, this, attachToRoot)
+}
+
+
+// region ViewModels
+
+/**
+ * For Actvities, allows declarations like
+ * ```
+ * val myViewModel = viewModelProvider(myViewModelFactory)
+ * ```
+ */
+inline fun <reified VM : ViewModel> FragmentActivity.viewModelProvider(
+        provider: ViewModelProvider.Factory
+) = ViewModelProviders.of(this, provider).get(VM::class.java)
+
+/**
+ * For Fragments, allows declarations like
+ * ```
+ * val myViewModel = viewModelProvider(myViewModelFactory)
+ * ```
+ */
+inline fun <reified VM : ViewModel> Fragment.viewModelProvider(
+        provider: ViewModelProvider.Factory
+) = ViewModelProviders.of(this, provider).get(VM::class.java)
+
+/**
+ * Like [Fragment.viewModelProvider] for Fragments that want a [ViewModel] scoped to the parent
+ * Fragment.
+ */
+inline fun <reified VM : ViewModel> Fragment.parentViewModelProvider(
+        provider: ViewModelProvider.Factory
+) = ViewModelProviders.of(parentFragment!!, provider).get(VM::class.java)
