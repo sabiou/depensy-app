eeec00ca650cd5510888f368cc8b5727ec808575
Farouk <sfarouk298@gmail.com>
2019-12-10 16:54:59 +0100

    Add ActivityScoped java interface

---
+++ core/src/main/java/dev/farouk/depensy/core/di/ActivityScoped.java
@@ -0,0 +1,13 @@
+package dev.farouk.depensy.core.di;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Documented
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityScoped {
+}
