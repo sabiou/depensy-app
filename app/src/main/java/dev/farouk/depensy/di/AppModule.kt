cde0e52c6b70badbc8419f892c867e7cbf8b6d6b
Farouk <sfarouk298@gmail.com>
2019-12-11 23:43:29 +0100

    Add AppModule class

---
+++ app/src/main/java/dev/farouk/depensy/di/AppModule.kt
@@ -0,0 +1,20 @@
+package dev.farouk.depensy.di
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dev.farouk.depensy.DepensyApplication
+import dev.farouk.depensy.core.data.db.ExpenseDatabase
+
+@Module
+class AppModule  {
+
+    @Provides
+    fun provideContext(application: DepensyApplication): Context {
+        return application.applicationContext
+    }
+
+    @Provides
+    fun providesAppDatabase(context: Context): ExpenseDatabase = ExpenseDatabase.buildDatabase(context)
+}
