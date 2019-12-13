4d5e1428f4ae9b893e0f7ba99540e751e6208609
Farouk <sfarouk298@gmail.com>
2019-12-13 20:41:42 +0100

    Add MainActivityViewModel

---
+++ app/src/main/java/dev/farouk/depensy/ui/MainActivityViewModel.kt
@@ -0,0 +1,7 @@
+package dev.farouk.depensy.ui
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class MainActivityViewModel @Inject constructor(context: Context): ViewModel()
