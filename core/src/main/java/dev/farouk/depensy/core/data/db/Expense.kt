c7b71c293f06ac0b2057322e016653d288227ccd
Farouk <sfarouk298@gmail.com>
2020-05-22 05:56:46 +0100

    - New Expense model

---
+++ core/src/main/java/dev/farouk/depensy/core/data/db/Expense.kt
@@ -0,0 +1,43 @@
+package dev.farouk.depensy.core.data.db
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import org.threeten.bp.LocalDate
+
+/**
+ * Created by Farouk on 16/05/20.
+ */
+@Parcelize
+data class Expense(
+        val id: String,
+        val amount: Double,
+        val title: String,
+        val date: LocalDate,
+        val notes: String,
+        val timestamp: Long?
+) : Parcelable, Comparable<Expense> {
+
+    /**
+     * Compares expenses based on date and timestamp.
+     */
+    override fun compareTo(other: Expense): Int {
+        return if (date == other.date) {
+            compareByTimestamp(other)
+        } else {
+            compareByDate(other)
+        }
+    }
+
+    private fun compareByTimestamp(other: Expense) = when {
+        // They are both the same.
+        timestamp == other.timestamp -> 0
+        // One has not been confirmed yet, so it's newer.
+        timestamp == null -> 1
+        // Other has not been confirmed yet, so it's newer.
+        other.timestamp == null -> -1
+        // Just compare values.
+        else -> timestamp.compareTo(other.timestamp)
+    }
+
+    private fun compareByDate(other: Expense) = date.compareTo(other.date)
+}
